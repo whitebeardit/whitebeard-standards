2b704696bd3cf17c75fc1c2d221f42038b0ca37e
almerindo <almerindo.rehem@gmail.com>
2023-02-26 23:14:55 -0300

    feat: add getEmpityValues into helpers

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,40 +1,52 @@
 import { Logger } from 'traceability';
 
+export const getEmptyValues = (environment: Object): string[] => {
+  const hasEmptyValues: string[] = [];
+
+  if (environment) {
+    Object.entries(environment).forEach(([key, value]) => {
+      if (key && !value) hasEmptyValues.push(key);
+    });
+  }
+
+  return hasEmptyValues;
+};
+
 export const limitMaxStringLength = (
-    string: string,
-    maxLength: number,
-  ): string => {
-    try {
-      const smallerString = string.slice(0, maxLength);
-      const isSmallerThenMaxLength = string.length > maxLength;
-      return `${smallerString}${isSmallerThenMaxLength ? '...' : ''}`;
-    } catch (error: any) {
-      Logger.error(error.message, {
-        eventName: 'limitMaxStringLength',
-        eventData: {
-          string,
-          maxLength,
-        },
-      });
-      throw error;
-    }
-  };
+  string: string,
+  maxLength: number,
+): string => {
+  try {
+    const smallerString = string.slice(0, maxLength);
+    const isSmallerThenMaxLength = string.length > maxLength;
+    return `${smallerString}${isSmallerThenMaxLength ? '...' : ''}`;
+  } catch (error: any) {
+    Logger.error(error.message, {
+      eventName: 'limitMaxStringLength',
+      eventData: {
+        string,
+        maxLength,
+      },
+    });
+    throw error;
+  }
+};
 
 export const decreaseAttributesLengthFromObject = (
-    object: any,
-    maxLength: number = 35,
-  ): Object => {
-    if (typeof object !== 'object') {
-      return object;
-    }
-  
-    let objectTransformed: any = {};
-    Object.keys(object).forEach((key) => {
-      objectTransformed[key] =
-        typeof object[key] == 'string'
-          ? limitMaxStringLength(object[key], maxLength)
-          : object[key];
-    });
-  
-    return objectTransformed;
-  };
+  object: any,
+  maxLength: number = 35,
+): Object => {
+  if (typeof object !== 'object') {
+    return object;
+  }
+
+  let objectTransformed: any = {};
+  Object.keys(object).forEach((key) => {
+    objectTransformed[key] =
+      typeof object[key] == 'string'
+        ? limitMaxStringLength(object[key], maxLength)
+        : object[key];
+  });
+
+  return objectTransformed;
+};
