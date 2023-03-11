7d9c734139aa3b6ae15e9f8b0b20e464adc77335
almerindo <almerindo.rehem@gmail.com>
2023-03-11 12:14:05 -0300

    feat: add utils gitconfiglocal

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,7 @@
 import { Logger } from 'traceability';
+import { promisify } from 'node:util';
+import process from 'node:process';
+import gitconfig from 'gitconfiglocal';
 
 export const getEmptyValues = (environment: Object): string[] => {
   const hasEmptyValues: string[] = [];
@@ -50,3 +53,20 @@ export const decreaseAttributesLengthFromObject = (
 
   return objectTransformed;
 };
+
+const pGitconfig = promisify(gitconfig);
+
+export default async function gitRemoteOriginUrl({
+  cwd = process.cwd(),
+  remoteName = 'origin',
+} = {}) {
+  const config = await pGitconfig(cwd);
+  const url =
+    config.remote && config.remote[remoteName] && config.remote[remoteName].url;
+
+  if (!url) {
+    throw new Error(`Couldn't find ${remoteName} url`);
+  }
+
+  return url;
+}
